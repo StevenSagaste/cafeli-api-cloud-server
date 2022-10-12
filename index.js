eb8e9d97dc193f01fc4145efc2fa9df7e1699865
Steven Sagaste <steven3140c@outlook.com>
2022-10-11 23:32:11 -0600

    cofigurating get and post endoints

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const axios = require("axios");
 const app = express().use(bodyParser.json()); // creates http server
-const token = 'VERIFICATION_TOKEN';
+//const token = 'VERIFICATION_TOKEN';
 const PORT = process.env.PORT || 3030;
 
 // your code
@@ -11,24 +12,55 @@ app.get('/webhook', (req, res) => {
     let mode=req.query['hub.mode']
     let challenge=req.query['hub.challenge']
     let verify_token=req.query['hub.verify_token']
+    let token=req.query['hub.verify_token']
     
-    if (req.query.verify_token !== token) {
-        return res.sendStatus(401);
+    if (mode && token) {
+        if (mode === 'subscribed' && verify_token === token) {
+            res.status(200).send(challenge);
+        } else {
+            
+            // return challenge
+            res.status(401).send(challenge);
+        }    
     }
-
-    // return challenge
-    return res.end(challenge);
 });
 
 app.post('/webhook', (req, res) => {
-    // check if verification token is correct
-    if (req.query.verify_token !== token) {
-        return res.sendStatus(401);
-    }
+    
     body_param=req.body;
     // print request body
     console.log(JSON.stringify(req.body,null,2));
+    if (body_param.object) {
+        if (body_param.entry &&
+            body_param.entry[0].challenges &&
+            body_param.entry[0].challenges[0].value.message &&
+            body_param.entry[0].challenges[0].value.message[0]
+            ) {
+            let phone_number_id = body_param.entry[0].challenges[0].value.metadata.phone_number_id;
+            let from = body_param.entry[0].challenges[0].value.messages[0].from;
+            let msg_body = body_param.entry[0].challenges[0].value.messages[0].text.body;
 
+            axios({
+                method: 'POST',
+                URL: `https://graph.facebook.com/v14.0/${phone_number_id}/messages?access_token=${token}`,
+                data: {
+                    messaging_product: 'whatsapp',
+                    to: from,
+                    text: {
+                        body: 'hello code'
+
+                    }
+                },
+                headers:{
+                    
+                    'Content-Type': 'application/json'
+                }
+            });
+            res.sendStatus(200);
+        }else{
+            res.sendStatus(404);
+        }
+    }
     // return a text response
     const data = {
         responses: [
@@ -39,7 +71,7 @@ app.post('/webhook', (req, res) => {
         ]
     };
 
-    res.json(data);
+    //res.json(data);
 });
 
 app.listen(PORT, () => {
