8d90a10437b434fe3ac4ec9ab156d34e5ac8cc7c
Steven Sagaste <steven3140c@outlook.com>
2022-10-11 23:54:30 -0600

    fixing post challenges to changes

--- index.js
+++ index.js
@@ -38,13 +38,13 @@ app.post('/webhook', (req, res) => {
     console.log(JSON.stringify(req.body,null,2));
     if (body_param.object) {
         if (body_param.entry &&
-            body_param.entry[0].challenges &&
-            body_param.entry[0].challenges[0].value.message &&
-            body_param.entry[0].challenges[0].value.message[0]
+            body_param.entry[0].changes &&
+            body_param.entry[0].changes[0].value.message &&
+            body_param.entry[0].changes[0].value.message[0]
             ) {
-            let phone_number_id = body_param.entry[0].challenges[0].value.metadata.phone_number_id;
-            let from = body_param.entry[0].challenges[0].value.messages[0].from;
-            let msg_body = body_param.entry[0].challenges[0].value.messages[0].text.body;
+            let phone_number_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
+            let from = body_param.entry[0].changes[0].value.messages[0].from;
+            let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
             axios({
                 method: 'POST',
